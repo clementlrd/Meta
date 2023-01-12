d3eb7e0437605d055275152aa1eb8983504054fb
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-01-12 11:57:34 +0100

    add responsivness

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,29 +1,38 @@
 <template>
-  <v-footer app padless absolute>
-    <v-card
-      flat
-      tile
-      class="indigo lighten-1 white--text text-center"
-      width="100%"
-      height="100%"
-    >
+  <v-footer app padless absolute v-if="$vuetify.breakpoint.xs">
+    <v-card tile class="indigo lighten-1 white--text text-center" width="100%">
+      <v-card-text class="white--text pa-1 pb-0 text-caption">
+        <strong>Liste BDE </strong><span class="hacked">Meta</span>
+        <strong> 2023 </strong>
+        <v-btn class="white--text" icon>
+          <v-icon size="14px"> mdi-facebook </v-icon>
+        </v-btn>
+        <v-btn class="white--text" icon>
+          <v-icon size="14px"> mdi-instagram </v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-card-text class="white--text pa-1 pt-0 text-caption">
+        Dev - Clément Laroudie
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  <v-footer app padless absolute v-else>
+    <v-card tile class="indigo lighten-1 white--text text-center" width="100%">
       <v-card-text class="white--text" no-gutters>
-        <v-row dense align="center">
-          <v-spacer />
-          <v-col>
+        <v-row dense align="center" justify="center">
+          <v-col class="text-right">
             <strong>Liste BDE </strong><span class="hacked">Meta</span>
             <strong> 2023 </strong>
           </v-col>
-          <v-col>
-            <v-btn class="mx-4 white--text" icon>
+          <v-col cols="2">
+            <v-btn class="white--text" icon>
               <v-icon size="24px"> mdi-facebook </v-icon>
             </v-btn>
-            <v-btn class="mx-4 white--text" icon>
+            <v-btn class="white--text" icon>
               <v-icon size="24px"> mdi-instagram </v-icon>
             </v-btn>
           </v-col>
-          <v-col> Développé par Clément Laroudie </v-col>
-          <v-spacer />
+          <v-col class="text-left"> Développé par Clément Laroudie </v-col>
         </v-row>
       </v-card-text>
     </v-card>
