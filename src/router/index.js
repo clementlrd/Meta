b3e657a2c1879561d30d6c7cedef3f2afeb20a41
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-01-13 10:15:56 +0100

    add planning

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 const Home = () => import('@/views/Home');
 const Trombi = () => import('@/views/Trombi');
 const Program = () => import('@/views/Program');
-//const Acti = () => import('@/views/Acti');
+const Acti = () => import('@/views/Acti');
 //const Goodies = () => import('@/views/Goodies');
 // Error page
 const Error404 = () => import('@/views/error pages/404');
@@ -29,7 +29,7 @@ const router = new VueRouter({
 		},
 		{
 			path: '/acti',
-			component: Futur,
+			component: Acti,
 		},
 		{
 			path: '/goodies',
--- src/views/Acti.vue
+++ src/views/Acti.vue
@@ -6,6 +6,17 @@
       style="height: auto; width: min(100%, 200px)"
       class="img_grow mt-4"
     />
+
+    <v-card
+      width="min(90%,900px)"
+      class="mx-auto mt-4 mb-2"
+      color="#ffffff00"
+      flat
+    >
+      <v-img src="@/assets/EDT.png"></v-img>
+    </v-card>
+
+    <!--
     <v-card width="min(90%,900px)" class="mx-auto">
       <v-row class="mt-12">
         <v-col :cols="$vuetify.breakpoint.xs ? 12 : 6">
@@ -20,6 +31,7 @@
         </v-col>
       </v-row>
     </v-card>
+    -->
   </v-container>
 </template>
 
