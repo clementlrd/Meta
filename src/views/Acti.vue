024fc597e0aaf30e9295a56b0e0f14cc1c6bcae0
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-01-14 15:44:12 +0100

    add flyer

--- src/views/Acti.vue
+++ src/views/Acti.vue
@@ -13,25 +13,33 @@
       color="#ffffff00"
       flat
     >
-      <v-img src="@/assets/EDT.png"></v-img>
+      <v-img src="@/assets/acti/EDT.png"></v-img>
     </v-card>
 
-    <!--
-    <v-card width="min(90%,900px)" class="mx-auto">
+    <v-card width="min(90%,900px)" class="mx-auto" color="#ffffff00" flat>
       <v-row class="mt-12">
         <v-col :cols="$vuetify.breakpoint.xs ? 12 : 6">
-          <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
+          <v-card
+            width="min(90%,900px)"
+            class="mx-auto mt-4 mb-2"
+            color="#ffffff00"
+            flat
+          >
+            <v-img src="@/assets/acti/affiche petite acti.jpg"></v-img>
           </v-card>
         </v-col>
         <v-col :cols="$vuetify.breakpoint.xs ? 12 : 6">
-          <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
+          <v-card
+            width="min(90%,900px)"
+            class="mx-auto mt-4 mb-2"
+            color="#ffffff00"
+            flat
+          >
+            <v-img src="@/assets/acti/affiche_soirée_bde_meta.jpg"></v-img>
           </v-card>
         </v-col>
       </v-row>
     </v-card>
-    -->
   </v-container>
 </template>
 
--- src/views/Goodies.vue
+++ src/views/Goodies.vue
@@ -8,26 +8,9 @@
     />
     <v-card width="min(90%,900px)" class="mx-auto">
       <v-row class="mt-12">
-        <v-col cols="6">
+        <v-col cols="6" v-for="card in cards" :key="card.name">
           <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row class="mt-8">
-        <v-col cols="6">
-          <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card width="min(90%,500px)" class="mx-auto">
-            <v-img src="@/assets/meta 600x600.png"></v-img>
+            <v-img :src="require(card_path + card_name + '.png')"></v-img>
           </v-card>
         </v-col>
       </v-row>
@@ -38,6 +21,75 @@
 <script>
 export default {
   name: "Goodies",
+  data() {
+    return {
+      hidden_card_pattern: "@/assets/goodies/carte_goodies_reverse_",
+      card_path: "@/assets/goodies/reveal/",
+      // 3 unique | 2 legendaire | 2 rare | 3 commun
+      cards: [
+        {
+          card_name: "stephane",
+          rarity: "unique",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "",
+          rarity: "unique",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "",
+          rarity: "unique",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "pommeau",
+          rarity: "legendaire",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "calendrier",
+          rarity: "legendaire",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "",
+          rarity: "rare",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "",
+          rarity: "rare",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "sticker",
+          rarity: "commun",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "sticker_collector",
+          rarity: "commun",
+          reveal: false,
+          place: "",
+        },
+        {
+          card_name: "sticker_rect",
+          rarity: "commun",
+          reveal: false,
+          place: "",
+        },
+      ],
+    };
+  },
 };
 </script>
 
