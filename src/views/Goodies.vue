aa97ce76c7fd8205522435c6af228394466cc68b
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-01-14 17:33:51 +0100

    add cards

--- src/views/Goodies.vue
+++ src/views/Goodies.vue
@@ -15,6 +15,7 @@
         >
           <v-card width="min(90%,500px)" class="mx-auto" color="#ffffff00" flat>
             <v-img :src="card.card"></v-img>
+            <strong class="hacked"> {{ card.place }} </strong>
           </v-card>
         </v-col>
       </v-row>
@@ -29,45 +30,49 @@ export default {
     return {
       // 3 unique | 2 legendaire | 2 rare | 3 commun
       cards: [
+        // unique
         {
           card: require("@/assets/goodies/reveal/stephane.png"),
-          place: "",
+          place: "Grosse activite",
         },
         {
-          card: require("@/assets/goodies/cartes_goodies_reverse_unique.png"),
-          place: "",
+          card: require("@/assets/goodies/cartes_goodies_reverse_unique.png"), // enceinte
+          place: "Grosse activite",
         },
         {
-          card: require("@/assets/goodies/cartes_goodies_reverse_unique.png"),
-          place: "",
+          card: require("@/assets/goodies/cartes_goodies_reverse_unique.png"), // Vélo custom META
+          place: "Grosse activite",
         },
+        // Légendaire
         {
           card: require("@/assets/goodies/reveal/pommeau.png"),
-          place: "",
+          place: "Petite activite",
         },
         {
           card: require("@/assets/goodies/reveal/calendrier.png"),
-          place: "",
+          place: "Petite activite",
         },
-        {
-          card: require("@/assets/goodies/cartes_goodies_reverse_rare.png"),
-          place: "",
-        },
-        {
-          card: require("@/assets/goodies/cartes_goodies_reverse_rare.png"),
-          place: "",
-        },
-        {
-          card: require("@/assets/goodies/reveal/sticker.png"),
-          place: "",
-        },
+        // Rare
         {
           card: require("@/assets/goodies/reveal/sticker_collector.png"),
-          place: "",
+          place: "?",
         },
         {
-          card: require("@/assets/goodies/reveal/sticker_rect.png"),
-          place: "",
+          card: require("@/assets/goodies/reveal/stickers_collector_rect.png"),
+          place: "?",
+        },
+        {
+          card: require("@/assets/goodies/cartes_goodies_reverse_rare.png"), // bande LEDs
+          place: "Grosse activite - Jeux quotidiens",
+        },
+        {
+          card: require("@/assets/goodies/cartes_goodies_reverse_rare.png"), // Bandana
+          place: "Grosse activite",
+        },
+        // commun
+        {
+          card: require("@/assets/goodies/reveal/sticker.png"),
+          place: "Stand",
         },
       ],
     };
@@ -75,5 +80,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.hacked {
+  font-family: "hackedregular";
+  font-size: 1.25em;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
 </style>
